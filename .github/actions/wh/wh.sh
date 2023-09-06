ac59576b11f98501487fcc66a2a30e76acecf33e
Dennis Mak <dmak@redhat.com>
2023-09-06 02:21:13 -0500

    test 18

--- .github/actions/wh/wh.sh
+++ .github/actions/wh/wh.sh
@@ -147,8 +147,8 @@ response=$(curl $options \
 
 CURL_STATUS=$?
 
-echo "response-body=$response" >> $GITHUB_OUTPUT
-# echo "response-body<<$REQUEST_ID" >> $GITHUB_OUTPUT
+# echo "response-body=$response" >> $GITHUB_OUTPUT
+echo "response-body<<$REQUEST_ID" >> $GITHUB_OUTPUT
 echo "$response" >> $GITHUB_OUTPUT
 echo "$REQUEST_ID" >> $GITHUB_OUTPUT
 
